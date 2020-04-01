010b0695b26ba2d1d7dd0c11fd1e331540dc39e2
Lorena Braghini <lorenabraghini@icloud.com>
2020-04-01 17:14:06 -0300

    Create Lugar.java

---
+++ Lugar.java
@@ -0,0 +1,15 @@
+public class Lugar {
+
+private int coord_x;
+private int coord_y;
+
+ public static Lugar(int coord_x, int coord_y){
+
+  this.coord_x = coord_x;
+  this.coord_y = coord_y;
+  
+  }  
+
+}
+
+  
