e365a98960da6ab1b20889b1b28c5f2568ad634e
Gustavo Palma <49881408+GustavoPalmaFig@users.noreply.github.com>
2020-04-01 17:25:43 -0300

    Update and rename Pessoa to Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,17 @@
+public class Pessoa extends Lugar {
+
+private int id;
+private int origem;
+private int destino;
+private int transf;
+
+ public static Pessoa(int id, int origem, int destino, int transf){
+
+  this.id = id;
+  this.origem = origem;
+  this.destino = destino;
+  this.transf = transf;
+  
+ }
+ 
+}
